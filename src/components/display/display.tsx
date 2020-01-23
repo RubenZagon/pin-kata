27fc0c1006e84f23797e0e62c4a22d33e454df82
Ruben Zagon <ruben.zagon@gmail.com>
2020-01-23 18:24:04 +0000

    Resuelto problemas de tipos

--- src/components/display/display.tsx
+++ src/components/display/display.tsx
@@ -3,13 +3,13 @@ import styled from '@emotion/styled';
 
 interface DisplayProps {
   className?: string;
+  text?: string
 }
 
-export const Display: FC<DisplayProps> = (props) => {
+export const Display: FC<DisplayProps> = ({ text }) => {
   return (
     <Container>
-
-
+      <h1>{text}</h1>
     </Container>
   );
 };
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,9 +1,9 @@
 import { combineReducers } from "redux";
-
+import displayNumber from './display';
 
 
 const pinPadApp = combineReducers({
-
+  displayNumber
 })
 
 export default pinPadApp
