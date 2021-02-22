60338e435551f2fa1cc933c76427d0e62d067e40
RubenZagon <ruben.zagon@gmail.com>
2021-02-22 11:02:56 +0000

    Deploying to gh-pages from @ RubenZagon/pin-kata@eb5560a1fc106666af8d00372faa0e98f083abc9 🚀

---
+++ precache-manifest.bca289f70c51d4d26fe6869481a568e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61473a44434ad341e762a7d54afc3330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c418f64a39232111bd1",
+    "url": "/static/css/main.e34498d8.chunk.css"
+  },
+  {
+    "revision": "157d9890f7dda0c7f37c",
+    "url": "/static/js/2.40910b2a.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/static/js/2.40910b2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c418f64a39232111bd1",
+    "url": "/static/js/main.65df79f7.chunk.js"
+  },
+  {
+    "revision": "50c1123d4c634d15c2a7",
+    "url": "/static/js/runtime-main.59bc8cc0.js"
+  }
+]);
